d0cdfc92e264e1b285ee43bd3eb659ff811fc7ab
Nithin RS <nithin007raj@gmail.com>
2024-12-30 16:13:15 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:earthquake_app/pages/home_page.dart';
+import 'package:earthquake_app/providers/app_data_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+      create: (ctx) => AppDataProvider(),
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark,),
+        useMaterial3: true,
+      ),
+      builder: EasyLoading.init(),
+      home: const HomePage(),
+    );
+  }
+}
+
